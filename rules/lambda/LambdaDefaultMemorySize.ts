c09effd2f8aba87cd6cbe80532a8af4570a71871
kevinwochan <kevinwochan@gmail.com>
2024-04-07 20:08:11 +1000

    fix: unused import

--- rules/lambda/LambdaDefaultMemorySize.ts
+++ rules/lambda/LambdaDefaultMemorySize.ts
@@ -3,7 +3,7 @@ Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
 import { parse } from 'path';
-import { CfnResource, Stack } from 'aws-cdk-lib';
+import { CfnResource } from 'aws-cdk-lib';
 import { CfnFunction } from 'aws-cdk-lib/aws-lambda';
 import { NagRuleCompliance } from 'cdk-nag';
 
